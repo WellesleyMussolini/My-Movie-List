5aa2f62ca859ae02e86c4d5d13ad9996a23680ad
WellesleyMussolini <iei.muss@gmail.com>
2022-07-11 16:25:43 -0300

    input-styled

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import ProductCard from "../../components/Card/Product-Card";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { HamburguerIcon, Wrapper } from "./home.styles";
+import { Header, HamburguerIcon, SearchContainer, InputText, SearchIconStyled, Wrapper } from "./home.styles";
 
 function Home() {
   const [openSidebar, setOpenSidebar] = useState(false);
 
   return (
     <>
-      <HamburguerIcon onClick={() => setOpenSidebar(true)} />
-      <Sidebar
-        openSidebar={openSidebar}
-        handleClose={() => setOpenSidebar(false)}
-      />
+      <Header>
+        <HamburguerIcon onClick={() => setOpenSidebar(true)} />
+        <Sidebar
+          openSidebar={openSidebar}
+          handleClose={() => setOpenSidebar(false)}
+        />
+        <SearchContainer>
+          <InputText type="search" placeholder="search..." />
+          <SearchIconStyled />
+        </SearchContainer>
+      </Header>
       <Wrapper>
         <ProductCard
           imgSrc="https://a-static.mlcdn.com.br/800x560/imac-27-apple-intel-core-i5-8gb-512gb-ssd-prateado/magazineluiza/226883300/b71738733735f8968100cbe86f589b30.jpg"
